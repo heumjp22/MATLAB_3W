08e6e36d30169013a34f6d543fa8b2b0c9749ea1
heumjp22 <heumjp22@gmail.com>
2026-03-20 22:55:28 +0900

    Add files via upload

---
+++ pjh_21010954_6.m
@@ -0,0 +1,23 @@
+bridge(1).location = 'Smith St.';
+bridge(1).maxLoad = 80;
+bridge(1).yearBuilt = 1928;
+bridge(1).yearLimit = 2025;
+bridge(2).location = 'Hope Ave.';
+bridge(2).maxLoad = 90;
+bridge(2).yearBuilt = 1950;
+bridge(2).yearLimit = 2027;
+bridge(3).location = 'Clark St.';
+bridge(3).maxLoad = 85;
+bridge(3).yearBuilt = 1933;
+bridge(3).yearLimit = 2024;
+bridge(4).location = 'North Rd.';
+bridge(4).maxLoad = 100;
+bridge(4).yearBuilt = 1960;
+bridge(4).yearLimit = 2023
+
+bridge(3).yearLimit = 2026
+
+bridge(5).location = 'Shore Rd.';
+bridge(5).maxLoad = 85;
+bridge(5).yearBuilt = 1997;
+bridge(5).yearLimit = 2022
